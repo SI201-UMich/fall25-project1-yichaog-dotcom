130276da68d3b546266bb3c12aa2358b9533b4ec
yichaog <xiyessita@gmail.com>
2025-10-15 13:18:05 -0400

    calculate average profit margin per category

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def load_csv(file):
     """
     Read the CSV file into a list of dictionaries.
     Parameters:
-        file (str): path to the CSV file
+        CSV file.
     Returns:
         list[dict]: dataset containing each row as a dictionary
     """
@@ -14,4 +14,58 @@ def load_csv(file):
         for row in reader:
             dataset.append(row)
     return dataset
+
+
+if __name__ == "__main__":
+    data = load_csv("SampleSuperstore.csv")
+    print(f"Total rows: {len(data)}")
+    print("Example row:", data[0])
+
+
+
+
+
+
+
+
+def calc_avg_profit_margin(data):
+    """
+    Calculate average profit margin per category.
+    Parameters:
+        Data from CSV.
+    Returns:
+        dict: {Category: average profit margin}
+    """
+    category_margin = {}   
+    category_count = {}    
+
+    for row in data:
+        try:
+            category = row["Category"]
+            sales = float(row["Sales"])
+            profit = float(row["Profit"])
+
+            if sales > 0:
+                margin = profit / sales
+                category_margin[category] = category_margin.get(category, 0) + margin
+                category_count[category] = category_count.get(category, 0) + 1
+        except (ValueError, KeyError):
+            continue  
+    avg_margin = {}
+    for category in category_margin:
+        avg_margin[category] = category_margin[category] / category_count[category]
+
+    return avg_margin
+
+if __name__ == "__main__":
+    data = load_csv("SampleSuperstore.csv")
+    results = calc_avg_profit_margin(data)
+    for k, v in results.items():
+        print(f"{k}: {v:.4f}")
+
+
+
+
+
+
 
